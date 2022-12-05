f503fba7a6b6b127e84b64f1f128506fa89db46b
wpdough <me@wpdough.com>
2022-12-05 14:49:36 -0700

    parse puzzle input

--- day05/part1.py
+++ day05/part1.py
@@ -15,6 +15,30 @@ def parse_stack_line(line):
     return vals
 
 
-for line in lines:
-    if "[" in line:
-        print(parse_stack_line(line))
+def parse_move_line(line):
+    line = line.replace("move ", "")
+    line = line.replace(" from", "")
+    line = line.replace(" to", "")
+    return [int(num) for num in line.split(" ")]
+
+
+class Input:
+    def __init__(self, stacks, moves):
+        self.stacks = stacks
+        self.moves = moves
+
+
+def parse_input(lines):
+    stacks = []
+    moves = []
+    for line in lines:
+        if "[" in line:
+            stacks.append(parse_stack_line(line))
+        if "move" in line:
+            moves.append(parse_move_line(line))
+    return Input(stacks, moves)
+
+
+input = parse_input(lines)
+print(input.stacks)
+print(input.moves)
